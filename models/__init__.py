9f9951f28dcd62eb5a3ad18137bfe206a5668e92
EggKai <67377160+EggKai@users.noreply.github.com>
2025-02-13 00:42:48 +0800

    changed relative import in models

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .AnglerUrl import checkUrls , is_downloadable, extractUrls
 from .AnglerLLM import LLM_probablity
-from .SpamAssassin import predict_spam_probability
+from .AnglerSpam import predict_spam_probability
 from .AnglerPhish import predict_phishing_probability
 from .AnglerMalware import predict_malware, TEXT_EXTENSIONS, get_file_extension
